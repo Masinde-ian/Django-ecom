9d1de4aa8c5ef49ee81744643f0beb1e34d01eca
Masinde-ian <hymasindeian@gmail.com>
2024-07-08 17:29:43 +0300

    c

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f9zt344*1h_h3h&+m6bq5b$ykh5ce(2o84c7x-9y5o!u*kb^i&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['projectryche-production.up.railway.app', 'https://projectryche-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://projectryche-production.up.railway.app']
